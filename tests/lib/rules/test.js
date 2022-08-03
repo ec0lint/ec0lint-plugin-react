d81278b6b7fde8a1cbf8fcc8ce3a141da53d9a45
malwinq <malwina.kubas@gmail.com>
2022-08-03 19:06:02 +0200

    fix eslint

--- tests/lib/rules/test.js
+++ tests/lib/rules/test.js
@@ -1,6 +1,12 @@
+'use strict';
+const assert = require('assert');
+const describe = require('describe');
+const it = require('it');
+
+
 describe('simple test', () => {
-    it('even simpler test', () => {
-        const trueObj = true;
-        assert.strictEqual(trueObj, true);
-      });
+  it('even simpler test', () => {
+    const trueObj = true;
+    assert.strictEqual(trueObj, true);
+  });
 });
